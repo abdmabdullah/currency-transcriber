c158d093685a8ba1c22026b318235f90fdf54454
abdmabdullah <abdmabdullah@gmail.com>
2023-09-06 00:32:05 +0500

    first iteration of algorithm

--- CurrencyTranscriber/CurrencyTranscriberApi/Controllers/CurrencyController.cs
+++ CurrencyTranscriber/CurrencyTranscriberApi/Controllers/CurrencyController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CurrencyTranscriberApi.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace CurrencyTranscriberApi.Controllers
 {
@@ -13,7 +15,38 @@ namespace CurrencyTranscriberApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult Get([FromQuery] string number)
         {
-            
+            if (number.Contains("."))
+                return BadRequest("invalid format");
+
+            number = Regex.Replace(number, @"\s", "");
+
+            decimal amount;
+
+            if (!decimal.TryParse(number.Replace(",", ""), out amount))
+                return BadRequest("Invalid format");
+
+            if (amount < 0 || amount > 999999999.99M)
+                return BadRequest("Invalid amount");
+
+            string[] splitNumber = number.Split(CurrencyHelper.DECIMAL_SEPARATOR);
+
+            int dollars = int.Parse(splitNumber[0]);
+            int cents = splitNumber.Length > 1 ? int.Parse(splitNumber[1]) : 0;
+
+            if (cents > 99)
+                return BadRequest("Invalid cents");
+
+            string dollarWords = CurrencyHelper.GetWords(dollars);
+            string centWords = CurrencyHelper.GetWords(cents);
+
+            string currencyInWords;
+
+            if (!string.IsNullOrWhiteSpace(centWords) && cents > 0)
+                currencyInWords = CurrencyHelper.GetFormattedCurrencyInWords(dollarWords, centWords);
+            else
+                currencyInWords = CurrencyHelper.GetFormattedCurrencyInWords(dollarWords);
+
+            return Ok(currencyInWords);
         }
     }
 }
