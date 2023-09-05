f68bf18efc88ff1fd4b3d4ea89313d8ed40e4410
abdmabdullah <abdmabdullah@gmail.com>
2023-09-05 20:04:40 +0500

    added empty endpoint

---
+++ CurrencyTranscriber/CurrencyTranscriberApi/Controllers/CurrencyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyTranscriberApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet(Name = "GetTranscribedCurrency")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Get([FromQuery] string number)
+        {
+            
+        }
+    }
+}
